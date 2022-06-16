2a5fc958bb3b63c7e5a300b3686c0275989f9daa
Artem Tikhonov <a.tikhonov@c-project.pro>
2022-06-16 11:29:39 +0300

    Добавил кастомную тему с рабочими цветами. Пока без типографии.

--- app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Color.kt
@@ -7,13 +7,13 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val Gray = Color(0xFFE6E6E6)
-val Blue = Color(0xFF2D69A6)
-
-val GreenStatus = Color(0xFF25AF3B)
-val GrayStatus = Color(0xFFA6ABA7)
-val YellowStatus = Color(0xFFF3C05B)
-val RedStatus = Color(0xFFFF0000)
-
-val GraySpaces = Color(0xFFE5E5E5)
-val Background = Color(0xFFFFFFFF) //TODO Удалить?
+val basePalette = AppealsComposeColors(
+    Gray = Color(0xFFE6E6E6),
+    Blue = Color(0xFF2D69A6),
+    GreenStatus = Color(0xFF25AF3B),
+    GrayStatus = Color(0xFFA6ABA7),
+    YellowStatus = Color(0xFFF3C05B),
+    RedStatus = Color(0xFFFF0000),
+    GraySpaces = Color(0xFFE5E5E5),
+    Background = Color(0xFFFFFFFF) ,
+)
--- app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Theme.kt
@@ -5,6 +5,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -28,7 +34,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun AppealsComposeTraineeProjectTheme(
+fun MainTheme(
+    textSize: AppealsComposeSize = AppealsComposeSize.Medium,
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
@@ -38,10 +45,50 @@ fun AppealsComposeTraineeProjectTheme(
         LightColorPalette
     }
 
+    val extendedTypography = AppealsComposeTypography(
+        heading = TextStyle(
+            fontSize = when (textSize) {
+                AppealsComposeSize.Small -> 24.sp
+                AppealsComposeSize.Medium -> 28.sp
+                AppealsComposeSize.Big -> 32.sp
+            },
+            fontWeight = FontWeight.Bold
+        ),
+        body = TextStyle(
+            fontSize = when (textSize) {
+                AppealsComposeSize.Small -> 14.sp
+                AppealsComposeSize.Medium -> 16.sp
+                AppealsComposeSize.Big -> 18.sp
+            },
+            fontWeight = FontWeight.Normal
+        ),
+        toolbar = TextStyle(
+            fontSize = when (textSize) {
+                AppealsComposeSize.Small -> 14.sp
+                AppealsComposeSize.Medium -> 16.sp
+                AppealsComposeSize.Big -> 18.sp
+            },
+            fontWeight = FontWeight.Medium
+        ),
+        caption = TextStyle(
+            fontSize = when (textSize) {
+                AppealsComposeSize.Small -> 10.sp
+                AppealsComposeSize.Medium -> 12.sp
+                AppealsComposeSize.Big -> 14.sp
+            }
+        )
+    )
+
     MaterialTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
+        content = content
+    )
+
+    CompositionLocalProvider(
+        LocalAppealsComposeColors provides basePalette,
+        LocalAppealsComposeTypography provides extendedTypography,
         content = content
     )
 }
