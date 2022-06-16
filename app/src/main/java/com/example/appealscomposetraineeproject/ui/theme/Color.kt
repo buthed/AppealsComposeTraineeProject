fbcaff64d11d9095af21452398593921af01cedc
Artem Tikhonov <a.tikhonov@c-project.pro>
2022-06-16 09:52:00 +0300

    Добавил цвета

--- app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/appealscomposetraineeproject/ui/theme/Color.kt
@@ -6,3 +6,14 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Gray = Color(0xFFE6E6E6)
+val Blue = Color(0xFF2D69A6)
+
+val GreenStatus = Color(0xFF25AF3B)
+val GrayStatus = Color(0xFFA6ABA7)
+val YellowStatus = Color(0xFFF3C05B)
+val RedStatus = Color(0xFFFF0000)
+
+val GraySpaces = Color(0xFFE5E5E5)
+val Background = Color(0xFFFFFFFF) //TODO Удалить?
