636331ab0ce7c133944840eb83c15b08527e5e22
Artem Tikhonov <a.tikhonov@c-project.pro>
2022-06-23 09:53:18 +0300

    Добавил фрагменты

---
+++ app/src/main/java/com/example/appealscomposetraineeproject/ui/screens/history/AppealHistoryFragment.kt
@@ -0,0 +1,22 @@
+package com.example.appealscomposetraineeproject.ui.screens.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class AppealHistoryFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                AppealHistoryScreen()
+            }
+        }
+    }
+}
